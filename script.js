8f90f7f93ed5a709ecd010c0c0e05a3accdd4c7b
thms200 <thms0504@gmail.com>
2019-09-19 16:45:32 +0900

    Modefied calender

--- script.js
+++ script.js
@@ -1,16 +1,17 @@
 let today = new Date(); // 오늘 날짜 생성
 let tbody = document.querySelector("tbody").children;
+let td = document.querySelectorAll("td");
 
 //이번달
 function makeCalender(now) {
   //이번달, 이번해 표기
   let weekYear = document.querySelector(".weekYear")
   let todayYear = today.getFullYear();
-  let todayMonth_Number = today.getMonth();
+  let todayMonth_number = today.getMonth();
   let todayMonth = "";
   let monthArr = ["January", "February", "March", "April", "May", "June", "July", "August", "September", "October", "November", "Decembar"]
   for(let i = 0; i < monthArr.length; i++) {
-    if(todayMonth_Number === i) {
+    if(todayMonth_number === i) {
       todayMonth = monthArr[i];
     };
   };
@@ -19,12 +20,12 @@ function makeCalender(now) {
   let oneDay = new Date(now.getFullYear(), now.getMonth(), 1); // 이번달 1일 생성
   let lastDay = new Date(now.getFullYear(), now.getMonth()+1, 0).getDate(); // 이번달 마지막일
 
-  let oneDay_Week_Number = oneDay.getDay(); // 이번달 1일 요일 생성 (숫자)
-  let oneDay_Week = ""; // 이번달 1일 요일 생성 (알파벳)
+  let oneDay_week_number = oneDay.getDay(); // 이번달 1일 요일 생성 (숫자)
+  let oneDay_week = ""; // 이번달 1일 요일 생성 (알파벳)
   let dayArr = ["Sun", "Mon", "Tue", "Wed", "Thu", "Fri", "Sat"];
   for(let i = 0; i < dayArr.length; i++) {
-    if(oneDay_Week_Number === i) {
-      oneDay_Week = dayArr[i];
+    if(oneDay_week_number === i) {
+      oneDay_week = dayArr[i];
     };
   };
 
@@ -34,7 +35,7 @@ function makeCalender(now) {
   //첫째주
   let day = 1; 
   for(let i = 0 ; i < firstWeek.length; i++) {
-    if(week[i].innerHTML === oneDay_Week) {
+    if(week[i].innerHTML === oneDay_week) {
       firstWeek[i].innerHTML = day;
       day++;
     } else {
@@ -96,3 +97,36 @@ previousMonth.addEventListener("click", function() {
   
     makeCalender(today);
 });
+
+//왼쪽에 오늘의 날짜, 요일 -> 원하는 날짜 선택하면 바뀔 부분
+(function() {
+  let selectDay = document.querySelector(".day");
+  selectDay.innerHTML = today.getDate();
+  
+  let selectWeek = document.querySelector(".week");
+  let selectWeek_number = today.getDay();
+  let selectWeek_value = "";
+  let dayArr = ["Sunday", "Monday", "Tuesday", "Wednesday", "Thursday", "Friday", "Saturday"];
+  for(let i = 0; i < dayArr.length; i++) {
+    if(selectWeek_number === i) {
+      selectWeek_value = dayArr[i];
+    };
+  };
+  selectWeek.innerHTML = selectWeek_value;selectWeek_value;
+
+  console.log(td[13].innerHTML, typeof td[13].innerHTML)
+  console.log(today.getDate(), typeof today.getDate())
+  for(let i = 0; i < td.length; i++) {
+    if(Number(td[i].innerHTML) === today.getDate()) {
+      td[i].classList.add("selectDate");
+    };
+  };
+})();
+
+//주말 글자색 표시
+(function(){
+  for(let i = 1; i < 7; i++) {
+    tbody[i].children[0].classList.add("red");
+    tbody[i].children[6].classList.add("blue");
+  };
+})();
